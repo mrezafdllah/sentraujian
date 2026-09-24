9606a00d8fe4bfaac034d8fe84c1594e07538f07
mrezafdllah <mreza.fadhilah88@gmail.com>
2026-09-24 15:31:23 +0700

    fix: use central api version constant

--- apps/central-api/cmd/central-api/main.go
+++ apps/central-api/cmd/central-api/main.go
@@ -14,7 +14,7 @@ func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	server := httpapi.New(httpapi.Config{Address: config.Address}, logger)
 
-	logger.Info("central api starting", "address", config.Address, "api_version", apiVersion)
+	logger.Info("central api starting", "address", config.Address, "api_version", httpapi.APIVersion)
 	if err := http.ListenAndServe(config.Address, server); err != nil {
 		logger.Error("central api stopped", "error", err)
 		os.Exit(1)
